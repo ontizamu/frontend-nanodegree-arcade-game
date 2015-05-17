e88ade4598615edefd81fc43c5a5d4b3e9818bae
Esther Ontiveros <eontiveros@yahoo.com>
2015-05-16 17:02:59 -0700

    Implement CheckCollisions as a separate function.

--- js/app.js
+++ js/app.js
@@ -6,22 +6,27 @@ var Enemy = function(x,y,speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
+    //Enemy x,y location
+    this.x = x; 
+    this.y = y; 
+    //Enemy limits
+    this.leftEnemy = x;
+    this.rightEnemy = x + 101;
+    this.topEnemy = y + 27;
+    this.bottomEnemy = y + 83;
+   
     this.speed = speed;
 };
 
-// Update the enemy's position, and handles collisions with the player.
+// Update the enemy's position and limits.
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-    var max_width = 505;
-    // Variables to define the limits of enemies and player
-    var leftEnemy, rightEnemy, topEnemy, bottomEnemy, leftPlayer, rightPlayer, topPlayer, bottomPlayer; 
+    var MAX_WIDTH = 505;
 
     //Check if the enemy has reached the end of the screen and if that is the case, move it back 
     //to the beginning
-    if (this.x >= max_width) {
+    if (this.x >= MAX_WIDTH) {
         this.x = 0;
     }
 
@@ -31,30 +36,28 @@ Enemy.prototype.update = function(dt) {
     
     this.x = this.x + (this.speed * dt);
 
-    //Assigned enemy and player's limits to variables.
+    //Update enemy limits
 
-    leftEnemy = this.x;
-    rightEnemy = this.x + 101;
-    topEnemy = this.y + 27;
-    bottomEnemy = this.y + 83;
-    leftPlayer = player.x + 25;
-    rightPlayer = player.x + 75;
-    topPlayer = player.y + 21;
-    bottomPlayer = player.y + 83;
-
-
-    // Check for collisions (Found the algorithm on the forums).
-    // If a collision occurs the game is reset and the score is decreased by 50 points.
-
-    if (! (leftEnemy > rightPlayer || rightEnemy < leftPlayer || topEnemy > bottomPlayer  || bottomEnemy < topPlayer )) {
-          player.reset();
-          score.total = score.total - 50;
-    }
-}
+    this.leftEnemy = this.x;
+    this.rightEnemy = this.x + 101;   
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Check for collisions (Found the algorithm on the forums).
+// If a collision occurs the game is reset and the score is decreased by 50 points.
+
+function checkCollisions () {
+    allEnemies.forEach(function(enemy) {
+        
+        if (! (enemy.leftEnemy > player.rightPlayer || enemy.rightEnemy < player.leftPlayer || enemy.topEnemy > player.bottomPlayer  || enemy.bottomEnemy < player.topPlayer )) {
+            player.reset();
+            score.total = score.total - 50;
+        }
+    });
 }
 
 // Now write your own player class
@@ -62,62 +65,75 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function (x,y) {
     this.sprite = 'images/char-cat-girl.png';
+    //Player x,y location
     this.x = x;
     this.y = y;
-}
+    //Player limits
+    this.leftPlayer = x + 25;
+    this.rightPlayer = x + 75;
+    this.topPlayer = y + 21;
+    this.bottomPlayer = y + 83;
 
-//Do not know what to put on this method. In my program the method that updates 
-//the position of the player is handleInput.
+};
+
+//Update player's limits.
 Player.prototype.update = function() {
-}
+    //Update player limits
+    this.leftPlayer =  player.x + 25;
+    this.rightPlayer = player.x + 75;
+    this.topPlayer = player.y + 21;
+    this.bottomPlayer = player.y + 83;
+};
 
 //Draw the player in screen.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-//This method is called when the player reaches the water. It moves
-//the player back to its initial location.
+//This method is called when the player reaches the water or when she collides with
+//an enemy. It moves the player back to its initial location.
 Player.prototype.reset = function() {
     this.x = 202;
     this.y = 415;
-}
+};
 
 //This method receives user input (the key that was pressed) and moves
 //the player accordingly. It also checks that the player does not 
 //move off screen and if the player reaches the water, it calls the 
 //reset method and add 100 to the score.
 Player.prototype.handleInput = function (key) {
-    var min_height = 0;
-    var min_width = 0;
-    var max_height = 415;
-    var max_width = 404;
-    var water_level = 83;
+    var MIN_HEIGHT = 0;
+    var MIN_WIDTH = 0;
+    var MAX_HEIGHT = 415;
+    var MAX_WIDTH = 404;
+    var REC_WIDTH = 101;
+    var REC_HEIGHT = 83;
+    var WATER_LEVEL = 83;
   
     switch (key) {
-        case 'left' :  if (this.x > min_width) {
-                        this.x = this.x - 101;
+        case 'left' :  if (this.x > MIN_WIDTH) {
+                        this.x = this.x - REC_WIDTH;
                        }
                        break;
-        case 'up' :    if (this.y > min_height) {
-                        if (this.y === water_level) {  //Player reaches the water
+        case 'up' :    if (this.y > MIN_HEIGHT) {
+                        if (this.y === WATER_LEVEL) {  //Player reaches the water
                             player.reset();
                             score.total = score.total + 100;
                         } else {
-                            this.y = this.y - 83;
+                            this.y = this.y - REC_HEIGHT;
                         }                     
                        };
                        break;
-        case 'right' : if (this.x < max_width) {
-                        this.x = this.x + 101;
+        case 'right' : if (this.x < MAX_WIDTH) {
+                        this.x = this.x + REC_WIDTH;
                        }
                        break;
-        case 'down' :  if (this.y < max_height) {
-                        this.y = this.y + 83;
+        case 'down' :  if (this.y < MAX_HEIGHT) {
+                        this.y = this.y + REC_HEIGHT;
                        }
                        break;
     }
-}
+};
 
 //Score Class, this class requires a render method 
 //to display the score in the right top corner.
@@ -125,7 +141,7 @@ Player.prototype.handleInput = function (key) {
 //are added, and when it collides with an enemy 50 points are deducted.
 var Score = function (total) {
     this.total = total;
-}
+};
 
 Score.prototype.render = function() {
     ctx.font = "36pt Impact";
@@ -134,7 +150,7 @@ Score.prototype.render = function() {
     ctx.strokeStyle = "white";
     ctx.lineWidth = 2;
     ctx.strokeText (this.total,454,90);
-}
+};                                                                                                       
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
